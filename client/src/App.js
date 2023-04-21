ab13f2bafc5a10d18607dd442b7003689da9e77f
hassimioudiallo <hddinguiraye@gmail.com>
2023-04-20 21:48:37 -0400

    added pages to the app

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import Home from './pages/Home';
 import Nav from './components/Nav';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Success from './pages/Success';
+
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -11,14 +15,38 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    // <ApolloProvider client={client}>
-      // <Nav />
-     <div>
-      Hello World
-     </div>
-    // </ApolloProvider> 
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Nav />
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+              <Route 
+                path="/success" 
+                element={<Success />} 
+              />
+            </Routes>
+          </StoreProvider>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
-
+    
+    
 export default App;
+  
+
 
